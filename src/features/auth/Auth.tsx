6c8a163005b8a240e233e978db5d2a59c380aba3
Jordan Orozco <joaorozc@bancolombia.com.co>
2023-02-26 03:11:18 -0500

    Nuevos ajustes

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import "./Auth.css"
+import Login from "./login/Login";
+import Register from "./register/Register";
 
 const Auth = () => {
+
+    const [isLogin, setIsLogin] = useState(true);
+
     return (
         <div className="content-auth">
-            
+            {isLogin ? <Login /> : <Register />}
         </div>
     )
 }
